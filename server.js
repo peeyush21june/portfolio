6e014efa59e7137c6e74aa346fc5a4fb92b98290
peeyush21june <piyushdwivedi21june@gmail.com>
2023-08-11 19:55:29 +0530

    no

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files from the build directory
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Handle requests for any path by serving the index.html file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
